4c39db913f586cd4dde6f0d6d0721811dacdf3e8
jaeseong90 <woavldjs12@gmail.com>
2022-04-06 14:57:52 +0900

    Updates

--- server/pages/index.js
+++ server/pages/index.js
@@ -29,26 +29,38 @@ function Seo({ title  }) {
 
 /***/ }),
 
-/***/ 369:
+/***/ 222:
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
+// ESM COMPAT FLAG
 __webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "default": () => (/* binding */ Home),
-/* harmony export */   "getServerSideProps": () => (/* binding */ getServerSideProps)
-/* harmony export */ });
-/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
-/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var styled_jsx_style__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(816);
-/* harmony import */ var styled_jsx_style__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(styled_jsx_style__WEBPACK_IMPORTED_MODULE_1__);
-/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(968);
-/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_head__WEBPACK_IMPORTED_MODULE_2__);
-/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(664);
-/* harmony import */ var next_router__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(853);
-/* harmony import */ var next_router__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(next_router__WEBPACK_IMPORTED_MODULE_4__);
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(689);
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_5__);
-/* harmony import */ var _Seo__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(790);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ Home),
+  "getServerSideProps": () => (/* binding */ getServerSideProps)
+});
+
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(997);
+// EXTERNAL MODULE: external "styled-jsx/style"
+var style_ = __webpack_require__(816);
+var style_default = /*#__PURE__*/__webpack_require__.n(style_);
+// EXTERNAL MODULE: external "next/head"
+var head_ = __webpack_require__(968);
+// EXTERNAL MODULE: ./node_modules/next/link.js
+var next_link = __webpack_require__(664);
+// EXTERNAL MODULE: external "next/router"
+var router_ = __webpack_require__(853);
+// EXTERNAL MODULE: external "react"
+var external_react_ = __webpack_require__(689);
+// EXTERNAL MODULE: ./pages/Seo.js
+var Seo = __webpack_require__(790);
+;// CONCATENATED MODULE: ./config.js
+const prefix =  true ? "https://jaeseong90.github.io/nextjsintro" : 0;
+
+;// CONCATENATED MODULE: ./pages/index.js
+
 
 
 
@@ -57,30 +69,30 @@ __webpack_require__.r(__webpack_exports__);
 
 
 function Home({ movies  }) {
-    const router = (0,next_router__WEBPACK_IMPORTED_MODULE_4__.useRouter)();
+    const router = (0,router_.useRouter)();
     const onClick = (id, title)=>{
         router.push(`/movies/${title}/${id}`);
     };
-    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+    return /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
         className: "jsx-1171378c2e934ab4" + " " + "container",
         children: [
-            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_Seo__WEBPACK_IMPORTED_MODULE_6__["default"], {
+            /*#__PURE__*/ jsx_runtime_.jsx(Seo["default"], {
                 title: "Home"
             }),
-            movies === null || movies === void 0 ? void 0 : movies.map((movie)=>/*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+            movies === null || movies === void 0 ? void 0 : movies.map((movie)=>/*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
                     onClick: ()=>onClick(movie.id, movie.original_title)
                     ,
                     className: "jsx-1171378c2e934ab4" + " " + "movie",
                     children: [
-                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("img", {
+                        /*#__PURE__*/ jsx_runtime_.jsx("img", {
                             src: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
                             className: "jsx-1171378c2e934ab4"
                         }),
-                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("h4", {
+                        /*#__PURE__*/ jsx_runtime_.jsx("h4", {
                             className: "jsx-1171378c2e934ab4",
-                            /*#__PURE__*/ children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(next_link__WEBPACK_IMPORTED_MODULE_3__["default"], {
+                            /*#__PURE__*/ children: /*#__PURE__*/ jsx_runtime_.jsx(next_link["default"], {
                                 href: `/movies/${movie.original_title}/${movie.id}`,
-                                children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                children: /*#__PURE__*/ jsx_runtime_.jsx("a", {
                                     className: "jsx-1171378c2e934ab4",
                                     children: movie.original_title
                                 })
@@ -89,7 +101,7 @@ function Home({ movies  }) {
                     ]
                 }, movie.id)
             ),
-            react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((styled_jsx_style__WEBPACK_IMPORTED_MODULE_1___default()), {
+            jsx_runtime_.jsx((style_default()), {
                 id: "1171378c2e934ab4",
                 children: ".container.jsx-1171378c2e934ab4{display:grid;grid-template-columns:1fr 1fr;padding:20px;gap:20px}.movie.jsx-1171378c2e934ab4{cursor:pointer}.movie.jsx-1171378c2e934ab4 img.jsx-1171378c2e934ab4{max-width:100%;-webkit-border-radius:12px;-moz-border-radius:12px;border-radius:12px;-webkit-transition:-webkit-transform.2s ease-in-out;-moz-transition:-moz-transform.2s ease-in-out;-o-transition:-o-transform.2s ease-in-out;transition:-webkit-transform.2s ease-in-out;transition:-moz-transform.2s ease-in-out;transition:-o-transform.2s ease-in-out;transition:transform.2s ease-in-out;-webkit-box-shadow:rgba(0,0,0,.1)0px 4px 12px;-moz-box-shadow:rgba(0,0,0,.1)0px 4px 12px;box-shadow:rgba(0,0,0,.1)0px 4px 12px}.movie.jsx-1171378c2e934ab4:hover img.jsx-1171378c2e934ab4{-webkit-transform:scale(1.05)translateY(-10px);-moz-transform:scale(1.05)translateY(-10px);-ms-transform:scale(1.05)translateY(-10px);-o-transform:scale(1.05)translateY(-10px);transform:scale(1.05)translateY(-10px)}.movie.jsx-1171378c2e934ab4 h4.jsx-1171378c2e934ab4{font-size:18px;text-align:center}"
             })
@@ -97,7 +109,7 @@ function Home({ movies  }) {
     });
 };
 async function getServerSideProps() {
-    const { results  } = await (await fetch(`http://localhost:3000/api/movies`)).json();
+    const { results  } = await (await fetch(`${prefix}/api/movies`)).json();
     return {
         props: {
             movies: results
@@ -255,7 +267,7 @@ module.exports = require("styled-jsx/style");
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = __webpack_require__.X(0, [895,664], () => (__webpack_exec__(369)));
+var __webpack_exports__ = __webpack_require__.X(0, [895,664], () => (__webpack_exec__(222)));
 module.exports = __webpack_exports__;
 
 })();
